a6c27245e0ab54b7c3851b1a279f027ca402599d
Anatoly Makeyev <anatoly.makeyev@gmail.com>
2025-11-05 13:41:44 +0200

    2

--- tests/example.spec.js
+++
@@ -1,9 +0,0 @@
-import { test, expect } from '@playwright/test';
-import LoginPage from '../pages/login';
-
-
-test('get started link', async ({ page }) => {
-  const loginPage = new LoginPage(page);
-  await loginPage.goto();
-  await loginPage.login(process.env.STANDARD_USER, process.env.PASSWORD);
-});
